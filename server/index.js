8dbacd74ee701bd0aa4d8f8498a62ad3acfbdff6
Ajith P Poulose <ajithpls2001@gmail.com>
2022-03-31 14:10:56 +0530

    some modifications

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3001, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(1337, () => {
+  console.log(`Example app listening on port 3001`)
 })
