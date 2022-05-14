da2eacfb58f07b696050384c30af6d49fe9fc1ef
Aashli <aashlianoopk.a@gmail.com>
2022-05-14 20:59:55 +0530

    added edit profile

--- client/src/components/Settings/FirstTab.js
+++ client/src/components/Settings/FirstTab.js
@@ -2,8 +2,24 @@ import React from "react";
 const FirstTab = () => {
   return (
     <div className="FirstTab">
-          <p>First Tab!! Hurray!!</p>
   
+       
+          <label for="pass">Username</label>
+          <input type="text" id="pass" name="username"
+           minLength="8" required></input>
+       
+
+    
+          <label for="mail">Email id</label>
+          <input type="email" id="mail" name="email"
+           minLength="8" required></input>
+
+          <label for="bio">Bio</label>
+
+          <textarea id="bio" name="bio" rows="3" cols="50">
+          </textarea>
+          <button className="button button1">Save Changes</button>
+      
   
     </div>
   );
