ed954fab59125efd79bb2784e03fb793c8c5cad9
ajithshine <ajithshine77@gmail.com.com>
2022-05-21 01:23:59 +0530

    ##feed

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,25 +1,43 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development';
 import feedcss from './Feed.module.css';
+import axios from 'axios';
 export default function Feed() {
+  const [post, setPost]= useState('')
+  const [abname, setAbname] = useState('')
+  const ann = localStorage.getItem('token')
+  const id = localStorage.getItem('id');
+  const url = 'http://localhost:3001/api/posts/'+ id;
+  const url2 = 'http://localhost:3001/api/users/'+ id;
+
+  console.log(ann);
+    axios.get(url,{
+      params:{token:ann}
+    }).then(
+      (response) => {
+        console.log(response.File.name);
+        setPost(response.data.username);
+      }
+    );  
+
+    axios.get(url2,{
+      params:{token:ann}
+    }).then(
+      (response) => {
+        console.log(response);
+        setAbname(response.data.username);
+      }
+    );
   return (
     <div>
         <div className={feedcss.outer}>
           <div className={feedcss.inner}>
             <div className={feedcss.box}>
                 {/* <img src={`data:image/png;base64,${product.img.data}`} /> */}
-            <div className={feedcss.naming}>abcd</div>
-            <div className={feedcss.post}></div>
+            <div className={feedcss.naming}>{abname}</div>
+            <div className={feedcss.post}>{}</div>
             </div>
-            <div className={feedcss.box}>
-                {/* <img src={`data:image/png;base64,${product.img.data}`} /> */}
-            <div className={feedcss.naming}>abcd</div>
-            <div className={feedcss.post}></div>
-            </div>
-            <div className={feedcss.box}>
-                {/* <img src={`data:image/png;base64,${product.img.data}`} /> */}
-            <div className={feedcss.naming}>abcd</div>
-            <div className={feedcss.post}></div>
-            </div>
+    
           </div>  
         </div>
     </div>
