b2684277d14a4130b65c8bb9ce673646896135f0
Ajith P Poulose <ajithpls2001@gmail.com>
2022-05-21 20:35:42 +0530

    Create Post Endpoint Updated

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       console.log(file.path);
-      cb(null, file.originalname)
+      cb(null, file.originalname + Date.now() + '.jpg')
     }
   })
    
