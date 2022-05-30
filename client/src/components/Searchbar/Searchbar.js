8775f1fac4cbc0620ac586c0c0502b1911c92566
ajithshine <ajithshine77@gmail.com.com>
2022-05-31 01:12:13 +0530

    ##search frontend code.

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
 import searchcss from "./Searchbar.module.css";
 import { FaHome } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
 import { IoNotificationsSharp } from "react-icons/io5";
 import { FaSignOutAlt } from "react-icons/fa";
+import axios from "axios";
+import Popup from "../PopUp/Popup";
 
 
 export default function Searchbar() {
   const navigate = useNavigate();
+  const [word, setWord] = useState('')
+  const [buttonPopup, setButtonPopup] = useState('false')
 
   const handleLogout = () => {
 		localStorage.removeItem("token");
 		// window.location.reload();
     navigate("/");
 	};
+
+  const search = async (e) =>
+  {
+    e.preventDefault();
+    try{
+      await axios.post('http://localhost:3001/api/users/search-users', word )
+      .then(function(response) {
+        console.log(response.data)
+        console.log("success")
+      });
+    }catch(error){
+      console.log("error");
+    }
+  }
+
+
   return (
     <div className={searchcss.outer}>
-      <form className={searchcss.bar}>
-        <button type="submit" className={searchcss.button}>
+      <form className={searchcss.bar} onSubmit={search}>
+        <input
+         className={searchcss.searchbox}
+         type="text" placeholder="Search.." 
+         name="search" 
+         value={word}
+         onChange={(e) => setWord(e.target.value)}
+         />
+        <button type="submit" className={searchcss.button} onClick={() => setButtonPopup(true)}>
           <FiSearch className={searchcss.icons} />
         </button>
-        <input className={searchcss.searchbox} type="text" placeholder="Search.." name="search"/>
       </form>
+
+
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        
+      </Popup>
+
 
      <Link to='/Dashboard' className={searchcss.hover}><FaHome className={`${searchcss.icon} ${searchcss.hover}`} /></Link>
       <div className={searchcss.hover}>
